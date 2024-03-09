d4f4422d6bb8aec760ce39852d9529558407c5e1
badr540 <badrbm540@gmail.com>
2024-03-09 11:12:38 +0300

    more pages and improved layout

--- apps/gamemodule/urls.py
+++ apps/gamemodule/urls.py
@@ -3,4 +3,6 @@ from apps.gamemodule import views
 
 urlpatterns = [    
     path('', views.index, name='index'),
+    path('leaderboard', views.leaderboard, name='leaderboard'),
+    path('about', views.about, name='about'),
 ]
--- apps/gamemodule/views.py
+++ apps/gamemodule/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 def index(request):
     # this view return index
 	return render(request, 'gamemodule/index.html')
+
+def leaderboard(request):
+    return render(request, 'gamemodule/leaderboard.html')
+
+def about(request):
+    return render(request, 'gamemodule/about.html')
